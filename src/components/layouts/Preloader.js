53af736400516d9ee6f85c129190f52dd5c0314f
Brian Sunday <brians931@gmail.com>
2020-07-02 10:57:24 +0300

    add preloader

---
+++ src/components/layouts/Preloader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Preloader = () => {
+  return (
+    <div class="progress blue">
+      <div class="indeterminate"></div>
+    </div>
+  );
+};
+
+export default Preloader;
