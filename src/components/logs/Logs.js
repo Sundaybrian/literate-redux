b07c2626686668b99f6ea92676c623498949f6ba
Brian Sunday <brians931@gmail.com>
2020-07-02 09:46:35 +0300

    add log item

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import LogItem from "./LogItem";
 
 const Logs = () => {
   const [logs, setLogs] = useState([]);
@@ -21,14 +22,14 @@ const Logs = () => {
     return <h4>Loading....</h4>;
   }
   return (
-    <ul className="collection-with-header">
+    <ul className="collection with-header">
       <li className="collection-header">
         <h4 className="center">Logs</h4>
       </li>
       {!loading && logs.length == 0 ? (
         <p className="center">No logs to display...</p>
       ) : (
-        logs.map((log) => <li>{log.message}</li>)
+        logs.map((log) => <LogItem log={log} key={log.id} />)
       )}
     </ul>
   );
